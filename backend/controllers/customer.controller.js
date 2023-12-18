f12a3ffac36299bc3fca4b82160e9095dbcb9f3b
rishi <rishisr.9044@gmail.com>
2023-12-18 15:01:47 +0530

    Initial commit

---
+++ backend/controllers/customer.controller.js
@@ -0,0 +1,55 @@
+const Customer = require("../models/customer.model.js");
+const resp = require("../utils/response.js");
+
+
+exports.create = (req, res) => {
+    if (!req.body) {
+        resp(res, 400, {}, "No data Present");
+    }
+
+
+    const customer = new Customer({
+        name: req.body.name,
+        age: req.body.age,
+        gender: req.body.gender,
+        status: req.body.status,
+        reg_date: req.body.reg_date,
+    });
+
+
+    Customer.create(customer, (err, data) => {
+        if (err)
+            resp(res, {}, 500, "Something went wrong");
+        else resp(res, data, 200, "Customer Added");
+    });
+
+};
+
+exports.completePayment = (req, res) => {
+    Customer.updateById(req.body.id, (err, data) => {
+        if (err) {
+            if (err.msg) {
+                resp(res, err, 404, "Entry not found");
+            } else {
+                resp(res, {}, 500, "Something went wrong");
+            }
+        } else {
+            resp(res, data, 200, "Payment Successful");
+        }
+    })
+
+}
+
+
+exports.getAllCustomers = (req, res) => {
+    Customer.getCustomers((err, data) => {
+        if (err) {
+
+            resp(res, {}, 500, "Something went wrong");
+
+        } else {
+            resp(res, data, 200, "Unpaid Customers");
+        }
+    })
+}
+
---
+++ backend/models/customer.model.js
@@ -0,0 +1,48 @@
+const sql = require("../dbConnect.js");
+const Customer = function (customer) {
+    this.name = customer.name;
+    this.age = customer.age;
+    this.gender = customer.gender;
+    this.status = customer.status;
+    this.reg_date = customer.reg_date;
+
+}
+
+Customer.create = (customer, result) => {
+    sql.query("INSERT INTO customer SET ?", customer, (err, res) => {
+        if (err) {
+            console.log("Error : ", err);
+            result(err, null);
+        } else {
+
+            console.log("Created Customer");
+            result(null, { id: res.insertId, ...customer });
+        }
+    })
+
+}
+
+Customer.updateById = (id, result) => {
+    sql.query("UPDATE customer SET status = ? where id=?", ["isPaid", id], (err, res) => {
+        if (err) {
+            console.log("Error : ", err);
+            result(err, null);
+        } else if (res.affectedRows == 0) {
+            result({ msg: "not_found" }, null);
+
+        } else {
+            result(null, { id: id, status: "isPaid" })
+        }
+    })
+}
+Customer.getCustomers = (result) => {
+    sql.query("SELECT * FROM customer where status = ?", "reg", (err, res) => {
+        if (err) {
+            console.log("Error : ", err);
+            result(err, null);
+        } else {
+            result(null, res);
+        }
+    })
+}
+module.exports = Customer;
